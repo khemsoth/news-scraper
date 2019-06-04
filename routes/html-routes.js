76247c0123ca62df5063f49a55fe93328d6df4ca
unknown <kevhemsoth@gmail.com>
2019-06-03 22:13:44 -0400

    Added some styling

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,35 +19,41 @@ router.get('/saved', function(req, res) {
 });
 
 
-router.get('/articles', function(req, res) {
+router.get('/scrape', function(req, res) {
+  db.News.remove({});
   axios.get('https://us.motorsport.com/').then(function(response) {
     var $ = cheerio.load(response.data);
     $('h3.ms-item_title').each(function(i, element) {
       var result = {};
       result.title =  $(this).text().trim();
-      result.link = $(this).children().attr('href');
+      result.link = $(this).children().attr('href');      
       db.News.create(result)
       .then(function(news) {
         console.log(news);
       })
+      }) 
+      res.redirect('articles')
+   
     })
   })
-  db.News.find({}).then(function(data) {
-    var stuff = {
-      articles: []
-    }
-    for(var i = 0; i < 20; i++) {
-      var newsTitle = data[i].title;
-      var newsLink = data[i].link;
-      var obj = {
-       title: newsTitle,
-       link: newsLink
+  router.get('/articles', function(req, res) {
+    db.News.find({}).then(function(data) {
+      var stuff = {
+        articles: []
       }
-      stuff.articles.push(obj);     
-  }
-    res.render('articles', stuff)
+      for(var i = 0; i < 20; i++) {
+        var newsTitle = data[i].title;
+        var newsLink = data[i].link;
+        var obj = {
+         title: newsTitle,
+         link: newsLink
+        }
+        stuff.articles.push(obj);     
+    }
+      res.render('articles', stuff)
   })
-})
+    })
+  
 
 router.get('*', function(req, res) {
   res.render('index');
